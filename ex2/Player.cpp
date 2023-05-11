b7f6eee3029ba763726a6d68db0a3031788cf5f2
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-05-11 23:23:00 +0300

    Added Files for Card and updated comments

--- ex2/Player.cpp
+++ ex2/Player.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "Player.h"
 
-/*
-* Constructor
-*/
+
 Player::Player(const char* name, int maxHp, int force) : maxHp(maxHp)
 {
 	this->name = new char[strlen(name) + 1];
@@ -14,9 +12,6 @@ Player::Player(const char* name, int maxHp, int force) : maxHp(maxHp)
 	this->coins = 0;
 }
 
-/*
-* Copy Constructor
-*/
 Player::Player(const Player& other) : maxHp(other.maxHp)
 {
 	this->name = new char[strlen(other.name) + 1];
@@ -27,9 +22,6 @@ Player::Player(const Player& other) : maxHp(other.maxHp)
 	this->coins = other.coins;
 }
 
-/*
-* Assignment Operator
-*/
 Player& Player::operator=(const Player& other)
 {
 	if (this != &other)
@@ -45,52 +37,31 @@ Player& Player::operator=(const Player& other)
 	return *this;
 }
 
-/*
-* Destructor
-*/
 Player::~Player()
 {
 	delete[] this->name;
 }
 
-/*
-* Prints the player's info
-* Uses the printPlayerInfo function from utilities.h
-*/
 void Player::printInfo() const
 {
 	printPlayerInfo(this->name, this->level, this->force, this->healthPoints, this->coins);
 }
 
-/*
-* Levels up the player
-* */
 void Player::levelUp()
 {
 	this->level++;
 }
 
-/*
-* @return the player's level
-*/
 int Player::getLevel()
 {
 	return this->level;
 }
 
-/*
-* Buffs the player's force
-* @param force - the amount of force to add
-*/
 void Player::buff(const int force)
 {
 	this->force += force;
 }
 
-/*
-* Heals the player
-* @param hp - the amount of hp to heal
-*/
 void Player::heal(const int hp)
 {
 	if (hp > 0)
@@ -103,10 +74,6 @@ void Player::heal(const int hp)
 	}
 }
 
-/*
-* Damages the player
-* @param hp - the amount of hp to damage
-*/
 void Player::damage(const int hp)
 {
 	if (hp > 0)
@@ -119,18 +86,11 @@ void Player::damage(const int hp)
 	}
 }
 
-/*
-* Checks if the player is knocked out
-*/
 bool Player::isKnockedOut() const
 {
 	return this->healthPoints == 0;
 }
 
-/*
-* Adds coins to the player
-* @param coins - the amount of coins to add
-*/
 void Player::addCoins(const int coins)
 {
 	if (coins > 0)
@@ -139,13 +99,6 @@ void Player::addCoins(const int coins)
 	}
 }
 
-/*
-* Pays coins from the player
-* @param coins - the amount of coins to pay
-* @return true if the player has enough coins to pay, false otherwise
-* if the player doesn't have enough coins, the function does nothing
-* if the player has enough coins, the function removes them from the player count
-*/
 bool Player::pay(const int coins)
 {
 	if (coins > 0 && this->coins >= coins)
@@ -156,9 +109,6 @@ bool Player::pay(const int coins)
 	return false;
 }
 
-/*
-* @return the player's attack strength
-*/
 int Player::getAttackStrength() const
 {
 	return this->force + this->level;
--- ex2/Player.h
+++ ex2/Player.h
@@ -5,27 +5,107 @@
 
 class Player
 {
-	char* name;
-	int level;
-	int force;
-	const int maxHp;
-	int healthPoints;
-	int coins;
+
 public:
+	/*
+	* Constructor of Player class
+	* @param name - The name of the player.
+	* @param maxHp - The maximum health points of the player.
+	* @param force - The force of the player.
+	* @return
+	*	     A new instance of Player.
+	*/
 	Player(const char* name, int maxHp = 100, int force = 5);
+
+	/*
+	* Copy Constructor of Player class
+	* @param other - The player to copy.
+	* @return
+	*      A new instance of Player.
+	*/
 	Player(const Player& other);
+
+	/*
+	* Destructor of Player class
+	*/
 	~Player();
+
+	/*
+	* Assignment Operator
+	* @param other - The player to copy.
+	* @return
+	*      A reference to this instance of Player.
+	*/
 	Player& operator=(const Player& other);
+
+	/*
+	* Prints the player's info
+	*/
 	void printInfo() const;
+
+	/*
+	* Level up the player
+	*/
 	void levelUp();
+
+	/*
+	* Get the level of the player
+	* @return
+	*      The level of the player.
+	*/
 	int getLevel();
-	void buff(const int force);
-	void heal(const int hp);
-	void damage(const int hp);
-	bool isKnockedOut() const;
-	void addCoins(const int coins);
-	bool pay(const int coins);
+
+	/*
+	* Calculates the attack strength of the player
+	* @return
+	*      The attack strength of the player.
+	*/
 	int getAttackStrength() const;
+
+	/*
+	* Buffs the player's force by 'force' points
+	* @param force - The number of points to buff.
+	*/
+	void buff(const int force);
+
+	/*
+	* Heals the player's health points by 'hp' points
+	* @param hp - The number of points to heal.
+	*/
+	void heal(const int hp);
+
+	/*
+	* Damages the player's health points by 'hp' points
+	* @param hp - The number of points to damage.
+	*/
+	void damage(const int hp);
+
+	/*
+	* Checks if the player is knocked out
+	* @return true if the player is knocked out, false otherwise.
+	*/
+	bool isKnockedOut() const;
+
+	/*
+	* Add coins to the player
+	* @param coins - The number of coins to add.
+	*/
+	void addCoins(const int coins);
+
+	/*
+	* Pay coins from the player
+	* @param coins - The number of coins to pay.
+	* @return true if the player has enough coins, false otherwise.
+	*/
+	bool pay(const int coins);
+
+private: 
+	char* name;
+	int maxHp;
+	int healthPoints;
+	int force;
+	int level;
+	int coins;
 };
 
 
