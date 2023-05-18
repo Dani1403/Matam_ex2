9af3de209d458964c811bf0044eceeff39e1e509
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-05-18 11:04:11 +0300

    Test completed and succeeded

--- ex2/Mtmchkin.cpp
+++ ex2/Mtmchkin.cpp
@@ -38,23 +38,3 @@ GameStatus Mtmchkin::getGameStatus() const
 		return GameStatus::MidGame;
 	}
 }
-
-int main()
-{
-	Card cards[4];
-	CardStats stats(3, 40, 10, 30, 1, 20);
-	cards[0] = Card(CardType::Treasure, stats);
-	cards[1] = Card(CardType::Buff, stats);
-	cards[2] = Card(CardType::Battle, stats);
-	cards[3] = Card(CardType::Heal, stats);
-	Mtmchkin game("Daniel", cards, 4);
-	while (!game.isOver())
-	{
-		game.playNextCard();
-	}
-	if (game.getGameStatus() == GameStatus::Win)
-	{
-		std::cout << "The Plater defeated all monsters";
-	}
-	return 0;
-}
